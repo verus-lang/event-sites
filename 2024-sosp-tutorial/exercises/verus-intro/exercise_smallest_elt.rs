b90f28d078c8730a181a1221bb2b2fbfbf0d165e
Andrea Lattuada <andrea@lattuada.me>
2024-11-02 13:54:54 -0500

    add verus-intro second exercise

---
+++ 2024-sosp-tutorial/exercises/verus-intro/exercise_smallest_elt.rs
@@ -0,0 +1,29 @@
+
+use vstd::prelude::*; verus! {
+
+fn smallest_elt(elts: Vec<u64>) -> (result: u64)
+  requires
+    elts@.len() > 0
+  ensures
+    forall|i:int| 0 <= i < elts@.len() ==> elts@[i] >= result,
+    exists|i:int| 0 <= i < elts@.len() && elts@[i] == result,
+{
+  let mut i = 1;
+  let mut smallest = elts[0];
+  while i < elts.len() 
+    invariant
+      1 <= i <= elts@.len(),
+      // fill in
+  {
+    if elts[i] < smallest {
+      smallest = elts[i];
+    }
+    i += 1;
+  }
+  smallest
+}
+
+fn main() {
+}
+
+}
