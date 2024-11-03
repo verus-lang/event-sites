88f8c90c1468631cfa734dd63911f7e1e03a8dc2
Andrea Lattuada <andrea@lattuada.me>
2024-11-03 13:23:17 -0600

    update fibo example

--- 2024-sosp-tutorial/demos/proof_debugging/fibo.rs
+++ 2024-sosp-tutorial/demos/proof_debugging/fibo.rs
@@ -1,6 +1,8 @@
 // vim: set filetype=verus laststatus=0:
 
-use vstd::prelude::*; verus! {
+use vstd::prelude::*;
+
+verus! {
 
 spec fn fibo_spec(n: nat) -> nat
   decreases n
@@ -27,12 +29,13 @@ proof fn lemma_fibo_is_monotonic(i: nat, j: nat)
   }
 }
 
-fn fibo(n: u64) -> (result: u64)
-  requires fibo_spec(n as nat) <= u64::MAX,
-  ensures result == fibo_spec(n as nat)
+exec fn fibo(n: u64) -> (result: Option<u64>)
+  ensures
+    result is Some <==> fibo_spec(n as nat) <= u64::MAX,
+    result matches Some(r) ==> fibo_spec(n as nat) == r,
 {
   if n == 0 {
-    return 0;
+    return Some(0);
   }
   let mut prev: u64 = 0;
   let mut cur: u64 = 1;
@@ -42,25 +45,37 @@ fn fibo(n: u64) -> (result: u64)
       0 < i <= n,
       prev == fibo_spec((i - 1) as nat),
       cur == fibo_spec(i as nat),
-      fibo_spec(n as nat) <= u64::MAX,
   {
     i = i + 1;
     proof {
       lemma_fibo_is_monotonic(i as nat, n as nat);
     }
-    assert(fibo_spec(i as nat) <= fibo_spec(n as nat));
-    assert(cur + prev <= u64::MAX);
-    let new_cur = cur + prev;
-    prev = cur;
-    cur = new_cur;
+    assert(cur + prev == fibo_spec(i as nat));
+    match cur.checked_add(prev) {
+      Some(new_cur) => {
+        prev = cur;
+        cur = new_cur;
+      },
+      None => {
+        assert(fibo_spec(n as nat) > u64::MAX);
+        return None;
+      },
+    }
   }
-  cur
+  Some(cur)
 }
 
-fn main() {
-  assert(fibo_spec(24) <= u64::MAX) by (compute);
-  let f = fibo(24);
-}
+// fn main() {
+//   assert(fibo_spec(16) <= u64::MAX) by (compute_only);
+//   let f = fibo(16);
+//   assert(fibo_spec(16) == 987) by (compute_only);
+//   assert(f == Some(987u64));
+// }
 
 } // verus!
 
+fn main() {
+  for i in 0..256 {
+    println!("fibo({}) == {:?}", i, fibo(i));
+  }
+}
